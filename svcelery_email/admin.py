828f0dc8d1c196cba0cca732dfb9f034cdd7fd42
tzangms <tzangms@gmail.com>
2012-10-24 05:38:18 +0000

    improve admin for MessageLog and bump to v0.3.8

--- svcelery_email/admin.py
+++ svcelery_email/admin.py
@@ -2,4 +2,7 @@ from django.contrib import admin
 from svcelery_email.models import Blacklist, MessageLog
 
 admin.site.register(Blacklist)
-admin.site.register(MessageLog)
+
+class MessageLogAdmin(admin.ModelAdmin):
+    list_display = ('email', 'result', 'created_at')
+admin.site.register(MessageLog, MessageLogAdmin)
