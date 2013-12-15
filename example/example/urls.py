ae90d900b68be617209dd37f3653d2b3638aaa22
tzangms <tzangms@gmail.com>
2013-12-15 20:40:59 +0800

    remove `example`

--- example/example/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import patterns, include, url
-
-from django.contrib import admin
-admin.autodiscover()
-
-urlpatterns = patterns('',
-    url(r'^djcelery_ses/', include('djcelery_ses.urls')),
-    url(r'^admin/', include(admin.site.urls)),
-)
