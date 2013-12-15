945b8c5fe63e6a70294265a5b0561277299b034d
tzangms <tzangms@gmail.com>
2013-12-15 09:15:35 +0800

    reinitialize south migrations

--- djcelery_ses/migrations/0001_initial.py
+++ djcelery_ses/migrations/0001_initial.py
@@ -9,40 +9,42 @@ class Migration(SchemaMigration):
 
     def forwards(self, orm):
         # Adding model 'Blacklist'
-        db.create_table(u'svcelery_email_blacklist', (
+        db.create_table(u'djcelery_ses_blacklist', (
             (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
             ('email', self.gf('django.db.models.fields.EmailField')(unique=True, max_length=75)),
+            ('type', self.gf('django.db.models.fields.PositiveSmallIntegerField')(default=0)),
             ('created_at', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
         ))
-        db.send_create_signal(u'svcelery_email', ['Blacklist'])
+        db.send_create_signal(u'djcelery_ses', ['Blacklist'])
 
         # Adding model 'MessageLog'
-        db.create_table(u'svcelery_email_messagelog', (
+        db.create_table(u'djcelery_ses_messagelog', (
             (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
             ('email', self.gf('django.db.models.fields.EmailField')(max_length=75)),
             ('body', self.gf('django.db.models.fields.TextField')()),
             ('result', self.gf('django.db.models.fields.CharField')(max_length=1)),
             ('created_at', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
         ))
-        db.send_create_signal(u'svcelery_email', ['MessageLog'])
+        db.send_create_signal(u'djcelery_ses', ['MessageLog'])
 
 
     def backwards(self, orm):
         # Deleting model 'Blacklist'
-        db.delete_table(u'svcelery_email_blacklist')
+        db.delete_table(u'djcelery_ses_blacklist')
 
         # Deleting model 'MessageLog'
-        db.delete_table(u'svcelery_email_messagelog')
+        db.delete_table(u'djcelery_ses_messagelog')
 
 
     models = {
-        u'svcelery_email.blacklist': {
+        u'djcelery_ses.blacklist': {
             'Meta': {'object_name': 'Blacklist'},
             'created_at': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
             'email': ('django.db.models.fields.EmailField', [], {'unique': 'True', 'max_length': '75'}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'})
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'type': ('django.db.models.fields.PositiveSmallIntegerField', [], {'default': '0'})
         },
-        u'svcelery_email.messagelog': {
+        u'djcelery_ses.messagelog': {
             'Meta': {'object_name': 'MessageLog'},
             'body': ('django.db.models.fields.TextField', [], {}),
             'created_at': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
@@ -52,4 +54,4 @@ class Migration(SchemaMigration):
         }
     }
 
-    complete_apps = ['svcelery_email']
+    complete_apps = ['djcelery_ses']
