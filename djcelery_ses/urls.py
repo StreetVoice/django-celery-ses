353472d0768fa3bc46bed75629911447bcda47df
Artin <artin@streetvoice.com>
2018-10-01 11:31:26 +0800

    url patterns

--- djcelery_ses/urls.py
+++ djcelery_ses/urls.py
@@ -1,5 +1,16 @@
-from django.conf.urls import url, patterns
 
-urlpatterns = patterns('djcelery_ses.views',
-    url(r'^sns_notification/$', 'sns_notification'),
-)
+import django
+
+from django.conf.urls import url, patterns
+from djcelery_ses import views
+
+
+if django.get_version() >= '1.8':
+    urlpatterns = [
+        url(r'^sns_notification/$', views.sns_notification),
+    ]
+else:
+    urlpatterns = patterns(
+        'djcelery_ses.views',
+        url(r'^sns_notification/$', 'sns_notification'),
+    )
