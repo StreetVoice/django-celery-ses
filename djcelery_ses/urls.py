9a8dd9749d2bfccbf681c31a511d31c174734f85
Ani <ani@streetvoice.com>
2026-09-14 18:45:15 +0800

    Drop supports before Django3.2

--- djcelery_ses/urls.py
+++ djcelery_ses/urls.py
@@ -1,26 +1,7 @@
+from django.urls import re_path
 
-import django
+from djcelery_ses import views
 
-
-if django.VERSION >= (3, 1):
-    from django.urls import re_path
-    from djcelery_ses import views
-
-    urlpatterns = [
-        re_path(r'^sns_notification/$', views.sns_notification),
-    ]
-
-elif django.VERSION >= (1, 9):
-    from django.conf.urls import url
-    from djcelery_ses import views
-
-    urlpatterns = [
-        url(r'^sns_notification/$', views.sns_notification),
-    ]
-
-else:
-    from django.conf.urls import url, patterns
-    urlpatterns = patterns(
-        'djcelery_ses.views',
-        url(r'^sns_notification/$', 'sns_notification'),
-    )
+urlpatterns = [
+    re_path(r'^sns_notification/$', views.sns_notification),
+]
